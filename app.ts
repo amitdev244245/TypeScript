e33d062440a3ba17438166389f92d368d5fa19c6
Amit Sharma <amitdev244245@gmail.com>
2023-04-21 01:26:58 +0530

    Started learning typescript

---
+++ app.ts
@@ -0,0 +1,34 @@
+// ------------------------------------------------
+// 👉JS
+// let num1 = "1";
+// function calculate(num1, num2) {
+//     return num1 + num2;
+// }
+// console.log(calculate(num1, 3));
+
+// 👉TS
+// let num1: number = 1;
+// function calculate(num1: number, num2: number) {
+//     return num1 + num2;
+// }
+// console.log(calculate(num1, 3));
+// ------------------------------------------------
+
+
+// ------------------------------------------------
+// 👉JS
+// function getTotal(numbers) {
+//     return number.reduce((acc, item) => {
+//         return acc + item;
+//     }, 0)
+// }
+// console.log(getTotal([3, 3, 2]));
+
+// 👉TS
+// function getTotal(numbers: Array<number>) {
+//     return numbers.reduce((acc, item) => {
+//         return acc + item;
+//     }, 0)
+// }
+// console.log(getTotal([3, 3, 2]));
+// ------------------------------------------------
